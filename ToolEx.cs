2bce5c5bf92d619cb8a41513fdac08a02aa2a337
Tao <LongTaoi@outlook.com>
2024-05-30 20:20:07 +0800

    添加项目文件。

---
+++ ToolEx.cs
@@ -0,0 +1,58 @@
+﻿using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.Geometry;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToolEx
+{
+    public static class Tools
+    {
+        static public ObjectId PostToModelSpace(this Database db, Entity ent)
+        {
+            if (ent == null) return ObjectId.Null;
+            ObjectId entId = ObjectId.Null;
+            using (Transaction trans = db.TransactionManager.StartTransaction())
+            {
+                BlockTable bt = trans.GetObject(db.BlockTableId, OpenMode.ForRead) as BlockTable;
+                BlockTableRecord btr = trans.GetObject(bt[BlockTableRecord.ModelSpace], OpenMode.ForWrite) as BlockTableRecord;
+                entId = btr.AppendEntity(ent);
+                trans.AddNewlyCreatedDBObject(ent, true);
+                trans.Commit();
+            }
+            return entId;
+        }
+
+        public static bool Transform(this ObjectId id, Matrix3d xform)
+        {
+            Entity ent = id.GetObject(OpenMode.ForWrite) as Entity;
+            if (ent != null)
+            {
+                ent.TransformBy(xform);
+                ent.DowngradeOpen();
+                return true;
+            }
+            return false;
+        }
+
+        public static bool Move(this ObjectId id, Vector3d vec) { return Transform(id, Matrix3d.Displacement(vec)); }
+
+        public static bool Move(this ObjectId id, Point3d source, Point3d target) {  return Move(id, target - source); }
+
+        public static bool Rotate(this ObjectId id, Point3d basePt, double angle) { return Transform(id, Matrix3d.Rotation(angle, Vector3d.ZAxis, basePt)); }
+
+        public static bool Scale(this ObjectId id, Point3d basePt, double scale) {  return Transform(id, Matrix3d.Scaling(scale, basePt)); }
+
+        public static bool Mirror(this ObjectId id, Line3d line) {  return Transform(id, Matrix3d.Mirroring(line));  }
+
+        public static bool Mirror(this ObjectId id, Point3d p1, Point3d p2) { return Mirror(id, new Line3d(p1, p2)); }
+
+        public static bool Mirror(this ObjectId id, Point3d basePoint) { return Transform(id, Matrix3d.Mirroring(basePoint));  }
+
+
+
+    }
+}
---
+++ myCommands.cs
@@ -0,0 +1,129 @@
+﻿// (C) Copyright 2024 by Home 
+//
+using App = Autodesk.AutoCAD.ApplicationServices ;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.EditorInput;
+using Autodesk.AutoCAD.Geometry;
+using Autodesk.AutoCAD.Runtime;
+using System;
+using System.Windows;
+using ToolEx;
+// This line is not mandatory, but improves loading performances
+[assembly: CommandClass(typeof(ArxDotNet.CommondDemo))]
+
+
+namespace ArxDotNet
+{
+
+    // This class is instantiated by AutoCAD for each document when
+    // a command is called by the user the first time in the context
+    // of a given document. In other words, non static data in this class
+    // is implicitly per-document!
+
+    public class CommondDemo
+    {
+        /// <summary>
+        /// 将实体添加到模型空间
+        /// </summary>
+        /// <param name="db"></param>
+        /// <param name="ent"></param>
+        /// <returns></returns>
+        [CommandMethod("TaoNet", nameof(ChangeColor), CommandFlags.Modal)]
+        public void ChangeColor() // This method can have any name
+        {
+            Database db = HostApplicationServices.WorkingDatabase;
+            Editor ed = App.Application.DocumentManager.MdiActiveDocument.Editor;
+            try
+            {
+                ObjectId id = ed.GetEntity("\n 请选择需要更改颜色得对象").ObjectId;
+
+                using (Transaction trans = db.TransactionManager.StartTransaction())
+                {
+                    Entity ent = trans.GetObject(id, OpenMode.ForWrite) as Entity;
+
+                    if (ent != null)
+                    {
+                        ent.ColorIndex = 6;
+                    }
+
+                    trans.Commit();
+
+                }
+
+            }
+            catch (Autodesk.AutoCAD.Runtime.Exception ex)
+            {
+                switch (ex.ErrorStatus)
+                {
+                    case ErrorStatus.InvalidIndex:
+                        ed.WriteMessage("\n 输入颜色异常！");
+                        break;
+                    case ErrorStatus.InvalidObjectId:
+                        ed.WriteMessage("\n 未选择对象！");
+                        break;
+                    default:
+                        ed.WriteMessage(ex.ErrorStatus.ToString());
+                        break;
+                }
+            }
+        }
+
+        // Modal Command with pickfirst selection
+        [CommandMethod("MyGroup", "MyPickFirst", "MyPickFirstLocal", CommandFlags.Modal | CommandFlags.UsePickSet)]
+        public void MyPickFirst() // This method can have any name
+        {
+            PromptSelectionResult result = App.Application.DocumentManager.MdiActiveDocument.Editor.GetSelection();
+            if (result.Status == PromptStatus.OK)
+            {
+                Editor ed = App.Application.DocumentManager.MdiActiveDocument.Editor;
+                ed.WriteMessage($"供选择 {0} 个实体", result.Value.Count);
+            }
+            else
+            {
+                Editor ed = App.Application.DocumentManager.MdiActiveDocument.Editor;
+                ed.WriteMessage("未选择对象");
+                MessageBox.Show("未选择对象!");
+            }
+        }
+
+        [CommandMethod(nameof(AddLine))]
+        public void AddLine()
+        {
+            Database db = HostApplicationServices.WorkingDatabase;
+            Point3d ptStart = new Point3d(0, 0, 0);
+            Point3d ptEnd = new Point3d(100, 100, 0);
+            Line line = new Line(ptStart, ptEnd);
+            db.PostToModelSpace(line);
+        }
+
+        [CommandMethod(nameof (SecondLine))]
+        public void SecondLine()
+        {
+            Database db = HostApplicationServices.WorkingDatabase;
+            Point3d ptStart = new Point3d(0, 100, 0);
+            Point3d ptEnd = new Point3d(0, 200, 0);
+            Line line = new Line(ptStart, ptEnd);
+            db.PostToModelSpace(line);
+        }
+
+        [CommandMethod(nameof(Demo))]
+        public void Demo()
+        {
+            Database db = HostApplicationServices.WorkingDatabase;
+            Editor ed = App.Application.DocumentManager.MdiActiveDocument.Editor;
+            ObjectId id = ed.GetEntity("\n 请选择需要移动对象").ObjectId;
+
+            if (id != null)
+            {
+                using (Transaction trans = db.TransactionManager.StartTransaction())
+                {
+                    id.Move(new Vector3d(100, 100, 0));
+                    trans.Commit();
+                }
+
+            }
+        }
+
+    }
+
+}
