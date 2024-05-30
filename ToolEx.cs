d91e9e4e1ebc6b452a007fdd7b7f63db7b0d03e8
Tao <LongTaoi@outlook.com>
2024-05-30 23:38:43 +0800

    添加一些函数

--- ToolEx.cs
+++ ToolEx.cs
@@ -11,7 +11,7 @@ namespace ToolEx
 {
     public static class Tools
     {
-        static public ObjectId PostToModelSpace(this Database db, Entity ent)
+        public static ObjectId PostToModelSpace(this Database db, Entity ent)
         {
             if (ent == null) return ObjectId.Null;
             ObjectId entId = ObjectId.Null;
@@ -26,8 +26,28 @@ namespace ToolEx
             return entId;
         }
 
+        public static ObjectIdCollection PostToModelSpace(this Database db, params Entity[] ents)
+        {
+            if (ents == null || ents.Length == 0) return new ObjectIdCollection();
+            ObjectIdCollection ids = new ObjectIdCollection();
+            using (Transaction tran = db.TransactionManager.StartTransaction())
+            {
+                BlockTable bt = tran.GetObject(db.BlockTableId, OpenMode.ForRead) as BlockTable;
+                BlockTableRecord btr = tran.GetObject(bt[BlockTableRecord.ModelSpace], OpenMode.ForWrite) as BlockTableRecord;
+                foreach (Entity ent in ents)
+                {
+                    ObjectId id = btr.AppendEntity(ent);
+                    tran.AddNewlyCreatedDBObject(ent, true);
+                    ids.Add(id);
+                }
+                tran.Commit();
+            }
+            return ids;
+        }
+
         public static bool Transform(this ObjectId id, Matrix3d xform)
         {
+            if (id.IsNull) return false;
             Entity ent = id.GetObject(OpenMode.ForWrite) as Entity;
             if (ent != null)
             {
@@ -46,13 +66,45 @@ namespace ToolEx
 
         public static bool Scale(this ObjectId id, Point3d basePt, double scale) {  return Transform(id, Matrix3d.Scaling(scale, basePt)); }
 
-        public static bool Mirror(this ObjectId id, Line3d line) {  return Transform(id, Matrix3d.Mirroring(line));  }
+        public static ObjectId Mirror(this ObjectId id, Line3d line, bool eraseSource = false)
+        { 
+            if (eraseSource)
+            {
+                Transform(id, Matrix3d.Mirroring(line));
+                return id;
+            }
+            else
+            {
+                return TransformCopy(id, Matrix3d.Mirroring(line));
+            }
+        }
 
-        public static bool Mirror(this ObjectId id, Point3d p1, Point3d p2) { return Mirror(id, new Line3d(p1, p2)); }
+        public static ObjectId Mirror(this ObjectId id, Point3d p1, Point3d p2, bool eraseSource = false) { return Mirror(id, new Line3d(p1, p2), eraseSource); }
 
-        public static bool Mirror(this ObjectId id, Point3d basePoint) { return Transform(id, Matrix3d.Mirroring(basePoint));  }
+        public static ObjectId Mirror(this ObjectId id, Point3d basePoint, bool eraseSource = false) 
+        { 
+            if (eraseSource)
+            {
+                Transform(id, Matrix3d.Mirroring(new Plane(basePoint, Vector3d.ZAxis)));
+                return id;
+            }
+            else
+            {
+                return TransformCopy(id, Matrix3d.Mirroring(new Plane(basePoint, Vector3d.ZAxis)));
+            }
+        }
 
-
+        public static ObjectId TransformCopy(this ObjectId id, Matrix3d xform)
+        {
+            if (id.IsNull) return ObjectId.Null;
+            Entity ent = id.GetObject(OpenMode.ForRead) as Entity;
+            if (ent != null)
+            {
+                Entity entCopy = ent.GetTransformedCopy(xform);
+                return ent.Database.PostToModelSpace(entCopy);
+            }
+            return ObjectId.Null;
+        }
 
     }
 }
